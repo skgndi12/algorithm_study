6ca47723a410825456a2067dd1fcd5506f99fa82
wnhlee <2wheeh@gmail.com>
2023-07-31 17:17:36 +0900

    fixup

--- baekjoon/divide_and_conquer/4779.js
+++ baekjoon/divide_and_conquer/4779.js
@@ -7,43 +7,16 @@ const solution = numbers => {
   }
 
   let ans = '';
-  for (const number of numbers) {
-    ans += result[number] + '\n';
-  }
+  for (const number of numbers) ans += result[number] + '\n';
 
   return ans;
-};
-
-class Cantor {
-  constructor() {
-    this.dp = Array(13).fill('');
-    this.dp[0] = '-';
-
-    for (let i = 1; i < 13; i++) {
-      this.dp[i] = this.dp[i - 1] + ' '.repeat(3 ** (i - 1)) + this.dp[i - 1];
-    }
-  }
-
-  draw = number => this.dp[number];
-}
-
-const solution2 = number => {
-  const cantor = new Cantor();
-
-  return cantor.draw(number);
 };
 
 function main() {
   const fs = require('fs');
   const input = fs.readFileSync('./baekjoon/divide_and_conquer/4779.txt').toString().trim().split('\n').map(Number);
 
-  // console.log(solution(input));
-  // console.log(solution2(input))
-  let ans = '';
-  for (const number of input) {
-    ans += solution2(number) + '\n';
-  }
-  console.log(ans);
+  console.log(solution(input));
 }
 
 if (require.main === module) {
